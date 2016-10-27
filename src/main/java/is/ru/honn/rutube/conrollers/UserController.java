b2e32e2c6126b83f2e287887ef25d766ba1e41e7
kari14@ru.is <karieiriks>
2016-10-27 13:37:05 +0000

    UserService and controller

--- src/main/java/is/ru/honn/rutube/conrollers/UserController.java
+++ src/main/java/is/ru/honn/rutube/conrollers/UserController.java
@@ -12,36 +12,47 @@ package is.ru.honn.rutube.conrollers;
 import is.ru.honn.rutube.domain.User;
 import is.ru.honn.rutube.domain.UserProfileChange;
 import is.ru.honn.rutube.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
- * Component description
+ * RESTful user controller
  *
  * @author Kári
  * @version 1.0, 26 okt. 2016
  */
+@RestController
+@RequestMapping("/user")
 public class UserController {
+
     UserService userService;
 
+    @Autowired
     public UserController(UserService userService){
         this.userService = userService;
     }
 
-    @RequestMapping(value = "/user/profile", method = RequestMethod.GET, consumes = MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(value = "/profile", method = RequestMethod.GET, consumes = MediaType.APPLICATION_JSON_VALUE)
     ResponseEntity getUserProfile(User user){
         return new ResponseEntity(HttpStatus.OK);
     }
 
-    @RequestMapping(value = "/user/profile", method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(value = "/profile", method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE)
     ResponseEntity modifyUserProfile(UserProfileChange userProfileChange){
         return new ResponseEntity(HttpStatus.OK);
     }
 
-    @RequestMapping(value = "/user/profile", method = RequestMethod.DELETE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    /**
+     *
+     * @param id The id of the user
+     * @return
+     */
+    @RequestMapping(value = "/profile", method = RequestMethod.DELETE, consumes = MediaType.APPLICATION_JSON_VALUE)
     ResponseEntity deleteUser(int id){
         if(userService.deleteUser(id)) {
             return new ResponseEntity(HttpStatus.OK);
@@ -49,7 +60,12 @@ public class UserController {
         return new ResponseEntity(HttpStatus.BAD_REQUEST);
     }
 
-    @RequestMapping(value = "/user/favoritevideo", method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE)
+    /**
+     *
+     * @param id The id of a video being added to favorites
+     * @return
+     */
+    @RequestMapping(value = "/favoriteVideo", method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE)
     ResponseEntity addVideoToFavorites(int id){
         if(userService.addVideoToFavorites(id)) {
             return new ResponseEntity(HttpStatus.OK);
@@ -57,7 +73,12 @@ public class UserController {
         return new ResponseEntity(HttpStatus.BAD_REQUEST);
     }
 
-    @RequestMapping(value = "/user/favoritevideo", method = RequestMethod.DELETE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    /**
+     *
+     * @param id The id of a video being deleted from favorites
+     * @return
+     */
+    @RequestMapping(value = "/favoriteVideo", method = RequestMethod.DELETE, consumes = MediaType.APPLICATION_JSON_VALUE)
     ResponseEntity deleteVideoFromFavorites(int id){
         if(userService.deleteVideoFromFavorites(id)) {
             return new ResponseEntity(HttpStatus.OK);
@@ -65,8 +86,12 @@ public class UserController {
         return new ResponseEntity(HttpStatus.BAD_REQUEST);
     }
 
-
-    @RequestMapping(value = "/user/closeFriends", method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE)
+    /**
+     *
+     * @param id The id of a user being added to close friends
+     * @return
+     */
+    @RequestMapping(value = "/closeFriends", method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE)
     ResponseEntity addCloseFriend(int id){
         if(userService.addUserToCloseFriends(id)) {
             return new ResponseEntity(HttpStatus.OK);
@@ -74,7 +99,12 @@ public class UserController {
         return new ResponseEntity(HttpStatus.BAD_REQUEST);
     }
 
-    @RequestMapping(value = "/user/closeFriends", method = RequestMethod.DELETE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    /**
+     *
+     * @param id The id of a user being deleted from close friends
+     * @return
+     */
+    @RequestMapping(value = "/closeFriends", method = RequestMethod.DELETE, consumes = MediaType.APPLICATION_JSON_VALUE)
     ResponseEntity deleteFromCloseFriends(int id){
         if(userService.deleteUserFromCloseFriends(id)) {
             return new ResponseEntity(HttpStatus.OK);
